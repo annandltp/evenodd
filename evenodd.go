019539e533ef39aa2c65eb598e8d898c41c408ad
annandl <anan.letcol123@gmail.com>
2022-06-13 23:30:52 +0700

    v2

--- evenodd.go
+++ evenodd.go
@@ -1,14 +1,20 @@
 package evenodd
 
-func CekGanjilGenap(i int) string {
+func CekGanjilGenap(num ...int) string {
 
 	result := ""
+	coma := ","
+	last := num[len(num)-1]
 
-	if i%2 == 0 {
-		result = "genap"
-	} else {
-		result = "ganjil"
+	for _, i := range num {
+		if last == i {
+			coma = ""
+		}
+		if i%2 == 0 {
+			result = "genap" + coma + " "
+		} else {
+			result = "ganjil" + coma + " "
+		}
 	}
-
 	return result
 }
